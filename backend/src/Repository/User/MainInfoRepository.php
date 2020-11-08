59f41133bce1fa52fbe914fad3716134a8427327
Jameson <riothewizard@gmail.com>
2020-11-08 23:14:09 +0500

    #3 main preparation backend

---
+++ backend/src/Repository/User/MainInfoRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repository\User;
+
+use App\Entity\User\MainInfo;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @method MainInfo|null find($id, $lockMode = null, $lockVersion = null)
+ * @method MainInfo|null findOneBy(array $criteria, array $orderBy = null)
+ * @method MainInfo[]    findAll()
+ * @method MainInfo[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MainInfoRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MainInfo::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
+    {
+        if (!$user instanceof MainInfo) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newEncodedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    // /**
+    //  * @return MainInfo[] Returns an array of MainInfo objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('m.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?MainInfo
+    {
+        return $this->createQueryBuilder('m')
+            ->andWhere('m.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
